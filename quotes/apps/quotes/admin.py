ffea3643b83850b2a586fe1330dff1e0e16f9d43
Volodymyr Zavatsky <volodymyr.zawatsky@gmail.com>
2023-10-06 16:10:09 +0300

    Fix model importing and registration.

--- quotes/apps/quotes/admin.py
+++ quotes/apps/quotes/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
-
-from django.contrib import admin
-from .models import Author, Quote, Tag, Tags
+from .models import Author, Quote, Tag
 
 # Register your models here.
+
 admin.site.register(Author)
 admin.site.register(Tag)
 admin.site.register(Quote)
