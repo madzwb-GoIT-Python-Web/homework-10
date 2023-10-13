ed6914a12864d9075c026f747fba6b8d237ff0bf
Volodymyr Zavatsky <volodymyr.zawatsky@gmail.com>
2023-10-13 18:55:42 +0300

    Change character fields' max length. Change 'description' field type of model 'Author' to TextField. Change 'born_date' field type of model 'Author' to DateField. Remove unnecessary unique constraint.

--- quotes/apps/quotes/models.py
+++ quotes/apps/quotes/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Logger(models.Model):
-    created_by    = models.ForeignKey(User, to_field="id" , on_delete=models.CASCADE, null=False, related_name="%(app_label)s_%(class)s_created_by")
-    modified_by   = models.ForeignKey(User, to_field="id" , on_delete=models.CASCADE, null=False, related_name="%(app_label)s_%(class)s_modified_by")
+    created_by    = models.ForeignKey(User, to_field="id" , on_delete=models.CASCADE, null=False, related_name="%(app_label)s_%(class)s_created_by",)# default=User.objects.get(pk=1))
+    modified_by   = models.ForeignKey(User, to_field="id" , on_delete=models.CASCADE, null=False, related_name="%(app_label)s_%(class)s_modified_by",)# default=User.objects.get(pk=1))
     created_at    = models.DateTimeField(null=False, auto_now_add =True)
     modified_at   = models.DateTimeField(null=False, auto_now     =True)
 
@@ -13,22 +13,22 @@ class Logger(models.Model):
             abstract = True
     
 class Author(Logger):
-    fullname        = models.CharField(max_length=128, null=False, unique=True)
-    born_date       = models.DateTimeField(null=False,)
-    born_location   = models.CharField(max_length=128, null=False, unique=True)
-    description     = models.CharField(max_length=1024, null=False, unique=True)
+    fullname        = models.CharField(max_length=256, null=False, unique=True)
+    born_date       = models.DateField(null=False,)
+    born_location   = models.CharField(max_length=1024, null=False)
+    description     = models.TextField(null=False)
 
     def __str__(self):
             return f"{self.fullname}"
 
 class Tag(Logger):
-    name = models.CharField(max_length=32, null=False, unique=True)
+    name = models.CharField(max_length=256, null=False, unique=True)
     
     def __str__(self):
             return f"{self.name}"
 
 class Quote(Logger):
-    quote   = models.CharField(max_length=1024, null=False, unique=True)
+    quote   = models.TextField(null=False, unique=True)
     # tags    = ArrayField(models.CharField(max_length=256, null=False, unique=True))
     tags = models.ManyToManyField(Tag)
 
